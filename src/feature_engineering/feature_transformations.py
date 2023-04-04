c76a8816e797dbdd512a2da80d29ee480426f4b1
chats-bug <71580082+chats-bug@users.noreply.github.com>
2023-04-04 21:58:52 +0530

    Feature Engineering for data

--- src/feature_engineering/feature_transformations.py
+++ src/feature_engineering/feature_transformations.py
@@ -1,30 +1,43 @@
 from ast import literal_eval
 import pandas as pd
 import numpy as np
-from geopy.geocoders import Nominatim
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+
 
 from src.utils.constants import INDEX, TARGET
-from src.utils.data_loader import DataLoader
 
 
-geolocator = Nominatim(user_agent="geoapiExercises")
-
-
-class FeatureEngineeringTransformations:
+class FeatureEngineeringTransformations(BaseEstimator, TransformerMixin):
 	"""
-	This class contains all the recommended feature engineeing transformations for the dataset.
+	This class contains all the recommended feature engineering transformations for the dataset.
+	
+	Parameters
+	----------
+		df: pd.DataFrame
+			The dataframe to be transformed
+		object_cols: list
+			The list of columns that contain the car features like top_features, comfort_features, etc. that need to be transformed
 	"""
-
+	
 	def __init__(
-		self, 
-		df: pd.DataFrame, 
-		object_cols: list[str] = ['top_features', 'comfort_features','interior_features', 'exterior_features', 'safety_features']
+			self,
+			object_cols=None,
+			target=TARGET
 	):
-		self.df = df
-		self.object_cols = object_cols
 		self.feature_prices = None
-		self.is_fitted = False
-
+		self.df = None
+		if object_cols is None:
+			object_cols = [
+				'top_features',
+				'comfort_features',
+				'interior_features',
+				'exterior_features',
+				'safety_features'
+			]
+		self.object_cols = object_cols
+		self.target = target
+	
 	def _car_object_feature_dict(self) -> dict:
 		unique_feature_scores = dict()
 		for col in self.object_cols:
@@ -33,12 +46,12 @@ class FeatureEngineeringTransformations:
 				for feature in feature_list:
 					if feature in unique_feature_scores.keys():
 						unique_feature_scores[feature][1] += 1
-						unique_feature_scores[feature][0] += row[TARGET]
+						unique_feature_scores[feature][0] += row[self.target]
 					else:
-						unique_feature_scores[feature] = [row[TARGET], 1]
-
+						unique_feature_scores[feature] = [row[self.target], 1]
+		
 		return unique_feature_scores
-
+	
 	def _map_object_cols_to_scores(self, x: str) -> float:
 		feature_list = literal_eval(x)
 		feature_score = 0
@@ -48,110 +61,27 @@ class FeatureEngineeringTransformations:
 			else:
 				feature_score += 0
 		return feature_score
-
+	
 	def _car_object_feature_transformation(self, df) -> pd.DataFrame:
 		if self.feature_prices is None:
 			raise Exception('Please fit the transformer first')
 		
 		for col in self.object_cols:
 			df[f'{col}_score'] = df[col].apply(self._map_object_cols_to_scores)
-
-			# Loop throught the rows with zero score and find a better score
-			# Most probably cause: the feature set is empty, not provided by the user
-			for _, zero_row in df[df[f'{col}_score'] == 0].iterrows():
-				zero_oem = zero_row['oem']
-				zero_model = zero_row['model']
-				zero_year = zero_row['myear']
-				zero_variant = zero_row['variant']
-
-				print(f'Zero score for {col} for {zero_oem} {zero_model} {zero_year} {zero_variant}')
-
-				# First check if the oem, model, year, variant are the same
-				condition = (self.df['oem'] == zero_oem) & (self.df['model'] == zero_model) & (self.df['myear'] == zero_year) & (self.df['variant'] == zero_variant) 
-				zero_row[f'{col}_score'] = self.df[condition][f'{col}_score'].mean()
-				# Update the score in the original dataframe
-				df.loc[zero_row.name, f'{col}_score'] = zero_row[f'{col}_score']
-				if zero_row[f'{col}_score'] != 0 and zero_row[f'{col}_score'] is not np.nan:
-					print(f'First check triggered {col} -> {zero_row[f"{col}_score"]}')
-					continue
-
-				# Second check if the oem, model, variant are the same
-				condition = (self.df['oem'] == zero_oem) & (self.df['model'] == zero_model) & (self.df['variant'] == zero_variant)
-				zero_row[f'{col}_score'] = self.df[condition][f'{col}_score'].mean()
-				# Update the score in the original dataframe
-				df.loc[zero_row.name, f'{col}_score'] = zero_row[f'{col}_score']
-				if zero_row[f'{col}_score'] != 0 and zero_row[f'{col}_score'] is not np.nan:
-					print(f'Second triggered {col} -> {zero_row[f"{col}_score"]}')
-					continue
-
-				# Third check if the oem, model, year are the same
-				condition = (self.df['oem'] == zero_oem) & (self.df['model'] == zero_model) & (self.df['myear'] == zero_year)
-				zero_row[f'{col}_score'] = self.df[condition][f'{col}_score'].mean()
-				# Update the score in the original dataframe
-				df.loc[zero_row.name, f'{col}_score'] = zero_row[f'{col}_score']
-				if zero_row[f'{col}_score'] != 0 and zero_row[f'{col}_score'] is not np.nan:
-					print(f'Third check triggered {col} -> {zero_row[f"{col}_score"]}')
-					continue
-
-				# Fourth check if the oem, model are the same
-				condition = (self.df['oem'] == zero_oem) & (self.df['model'] == zero_model)
-				zero_row[f'{col}_score'] = self.df[condition][f'{col}_score'].mean()
-				# Update the score in the original dataframe
-				df.loc[zero_row.name, f'{col}_score'] = zero_row[f'{col}_score']
-				if zero_row[f'{col}_score'] != 0 and zero_row[f'{col}_score'] is not np.nan:
-					print(f'Fourth check triggered {col} -> {zero_row[f"{col}_score"]}')
-					continue
-
-				# Fifth check if the oem, year are the same
-				condition = (self.df['oem'] == zero_oem) & (self.df['myear'] == zero_year)
-				zero_row[f'{col}_score'] = self.df[condition][f'{col}_score'].mean()
-				# Update the score in the original dataframe
-				df.loc[zero_row.name, f'{col}_score'] = zero_row[f'{col}_score']
-				if zero_row[f'{col}_score'] != 0 and zero_row[f'{col}_score'] is not np.nan:
-					print(f'Fifth check triggered {col} -> {zero_row[f"{col}_score"]}')
-					continue
-
-				# Sixth check if the oem is the same
-				condition = (self.df['oem'] == zero_oem)
-				zero_row[f'{col}_score'] = self.df[condition][f'{col}_score'].mean()
-				# Update the score in the original dataframe
-				df.loc[zero_row.name, f'{col}_score'] = zero_row[f'{col}_score']
-				if zero_row[f'{col}_score'] != 0 and zero_row[f'{col}_score'] is not np.nan:
-					print(f'Sixth check triggered {col} -> {zero_row[f"{col}_score"]}')
-					continue
-		
+			df.drop(col, axis=1, inplace=True)
+			# Replace zero scores with nan
+			df[f'{col}_score'] = df[f'{col}_score'].replace(0, np.nan)
 		return df
-
-	def _get_lat_lon(self, row: pd.Series) -> str:
-		location = geolocator.geocode(f'{row["loc"]} {row["City"]} {row["state"]}')
-		if location is None:
-			location = geolocator.geocode(f'{row["City"]} {row["state"]}')
-		(lat, lon) = (location.raw['display_name']['lat'], location.raw['display_name']['lon']) if location else (np.nan, np.nan)
-		return f'{lat},{lon}'
-
-	def _location_transformation(self, df: pd.DataFrame) -> pd.DataFrame:
-		# Use geopy to get the zip code from the location: 'loc' + 'City' + 'state' -> 'zip'
-		df['lat_lon'] = df.apply(self._get_lat_lon, axis=1)
-		df['lat'] = df['lat_lon'].apply(lambda x: x.split(',')[0])
-		df['lon'] = df['lat_lon'].apply(lambda x: x.split(',')[1])
-		df.drop('lat_lon', axis=1, inplace=True)
-		return df
-
-	def fit(self):
+	
+	def fit(self, X: pd.DataFrame, y=None) -> 'FeatureEngineeringTransformations':
+		self.df = X.copy()
 		self.feature_prices = self._car_object_feature_dict()
-		self.df = self._car_object_feature_transformation(self.df)
-		self.is_fitted = True
 		return self
-
-	def transform(self, df: pd.DataFrame) -> pd.DataFrame:
-		if self.is_fitted == False:
-			raise Exception('Please fit the transformer first')
-		# Transform the object columns to scores
-		df = self._car_object_feature_transformation(df)
-		df.drop(self.object_cols, axis=1, inplace=True)
-
-		# Transform the location to zip code
-		# df = self._location_transformation(df)
-		# We temporarily suspend fetching the zip code from the location because it takes too long and the result isn't accurate enough
+	
+	def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+		check_is_fitted(self, 'feature_prices')
 		
-		return df
+		# Transform the object columns to scores
+		X = X.copy()
+		X = self._car_object_feature_transformation(X)
+		return X
